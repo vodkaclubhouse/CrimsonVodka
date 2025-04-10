a9e8300e6b18f7d96f4761e33de81e6d2cdc38fb
Vodka Club House 🍷 <vodkaclubhouse@gmail.com>
2025-04-10 20:43:36 +0200

    Add files via upload

---
+++ webview/TS3Status.php
@@ -0,0 +1,10 @@
+<body background="orig_234758.jpg">
+<div id="ts3viewer_1099174" style="width:auto; height:auto; padding:0%; "> </div>
+
+<script src="https://static.tsviewer.com/short_expire/js/ts3viewer_loader.js"></script>
+<script>
+var ts3v_url_1 = "https://www.tsviewer.com/ts3viewer.php?ID=1099174&text=000000&text_size=13&text_family=6&text_s_color=000000&text_s_weight=normal&text_s_style=normal&text_s_variant=normal&text_s_decoration=none&text_i_color=&text_i_weight=normal&text_i_style=normal&text_i_variant=normal&text_i_decoration=none&text_c_color=&text_c_weight=normal&text_c_style=normal&text_c_variant=normal&text_c_decoration=none&text_u_color=000000&text_u_weight=normal&text_u_style=normal&text_u_variant=normal&text_u_decoration=none&text_s_color_h=&text_s_weight_h=bold&text_s_style_h=normal&text_s_variant_h=normal&text_s_decoration_h=none&text_i_color_h=000000&text_i_weight_h=bold&text_i_style_h=normal&text_i_variant_h=normal&text_i_decoration_h=none&text_c_color_h=&text_c_weight_h=normal&text_c_style_h=normal&text_c_variant_h=normal&text_c_decoration_h=none&text_u_color_h=&text_u_weight_h=bold&text_u_style_h=normal&text_u_variant_h=normal&text_u_decoration_h=none&iconset=default_colored_2014";
+ts3v_display.init(ts3v_url_1, 1099174, 100);
+</script>
+<p style="color:red;"class="text-capitalize"><img src="logo.png" alt="Smiley face" height="16" width="16"> <b>Powered by <a href="http://facebook.com/CIAForces">CIA Forces Clan</b></a></a></p></center>
+</body>
